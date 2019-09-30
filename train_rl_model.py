854ccf0fb22d59150ce5c9810026feb496de021c
Chris <chrispcharlton@gmail.com>
2019-10-01 08:08:21 +1300

    Added a few lines to plot reward/loss after rl training

--- train_rl_model.py
+++ train_rl_model.py
@@ -1,7 +1,15 @@
 from reinforcement_learning import train, chat
 
 # train model using defaults (warm-started s2s, 50 eps)
-policy, env,  total_rewards, dqn_losses = train(num_episodes=1)
+policy, env,  total_rewards, dqn_losses = train(num_episodes=100)
 
 # evaluate trained model
 chat(policy, env)
+
+import matplotlib.pyplot as plt
+
+plt.plot(total_rewards)
+plt.show()
+plt.clf()
+plt.plot(dqn_losses)
+plt.show()
