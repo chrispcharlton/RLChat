3328010da840a918525852fc895381ff7a270b0a
Chris <chrispcharlton@gmail.com>
2019-10-01 13:07:33 +1300

    Switched save and test order in training loop

--- seq2seq/train.py
+++ seq2seq/train.py
@@ -8,7 +8,6 @@ from seq2seq.trainingMethods import trainIters
 
 corpus_name = "cornell movie-dialogs corpus"
 corpus = os.path.join("data", corpus_name)
-corpus = os.path.join("C:\\Users\\Christopher\\PycharmProjects\\RLChat","data", corpus_name)
 # Define path to new file
 datafile = os.path.join(corpus, "formatted_movie_lines.txt")
 
