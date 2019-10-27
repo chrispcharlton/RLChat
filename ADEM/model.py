3769f1a2e22bf0e87b9d2a11965f2de1fd6db4fd
Chris <chrispcharlton@gmail.com>
2019-10-27 14:26:23 +1300

    Added additional linear layer to adem model

--- ADEM/model.py
+++ ADEM/model.py
@@ -32,7 +32,8 @@ class ADEM(nn.Module):
         #   because our input size is a word embedding with number of features == hidden_size
         self.gru = nn.GRU(hidden_size, hidden_size, n_layers,
                           dropout=(0 if n_layers == 1 else dropout), bidirectional=True)
-        self.fc = nn.Linear(hidden_size, output_size)
+        self.fc1 = nn.Linear(hidden_size*2, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, state, hidden=None):
         # Convert word indexes to  embeddings
@@ -47,9 +48,8 @@ class ADEM(nn.Module):
         # Forward pass through GRU
         self.gru.flatten_parameters()
         output, hidden = self.gru(packed, hidden)
-        output, _ =  nn.utils.rnn.pad_packed_sequence(output)
-        output = output[:, :, :self.hidden_size] + output[:, :, self.hidden_size:]
-        output = self.fc(output)
+        output = self.fc1(torch.cat([hidden[0], hidden[1]], dim=1))
+        output = self.fc2(output)
         # Unpack padding
         # outputs, _ = nn.utils.rnn.pad_packed_sequence(fc_output)
         # Sum bidirectional GRU outputs
@@ -65,7 +65,6 @@ class ADEM(nn.Module):
         :param hidden:
         :return: number 0 - 4 corresponding to rating from Alexa dataset
         '''
-
         pred = self(state, hidden)
         return pred.data.max(1, keepdim=True)[1]
 
